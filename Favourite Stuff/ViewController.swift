40a4cb396664eec03db11b85f5cdf38158e3a49c
Timothy Arquitt <timothyarquitt@timothys-macbook-pro-2.local>
2019-04-03 23:54:30 -0500

    version alpha

--- Favourite Stuff/ViewController.swift
+++ Favourite Stuff/ViewController.swift
@@ -8,13 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    //Variables
+    
+    var booksLoaded = false
+    var booksOrMovies = 1
+    
+    //Data set
+    
+    var dataForMovies = ["Return of the King", "Star Wars", "No Country for Old Men", "Scott Pilgrim", "Metropolis"]
+    var dataForBooks = ["The Hobbit", "Prince Caspian", "Dreams in the Witch House", "Iron Shadows in the Moon", "Out of the Silent Planet"]
+    
+    //-----Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var buttonLabel1: UIButton!
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    //viewDidLoad sets datasource
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        myTableView.dataSource = self
+        
+}
+    
+    
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if booksLoaded == false {
+            return dataForMovies.count
+        }
+        if booksLoaded == true {
+            return dataForBooks.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "cellInTabel")
+        
+        
+        if booksLoaded == false {
+            let holderText = dataForMovies[indexPath.row]
+            myCell?.textLabel?.text = holderText
+        }
+        if booksLoaded == true {
+            let holderTextTwo = dataForBooks[indexPath.row]
+            myCell?.textLabel?.text = holderTextTwo
+        }
+        
+        return myCell!
+    }
+    
+    //Button func
+    
+   
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        //checks if books are already showing
+        switch booksOrMovies {
+        case 1:
+            booksLoaded = true
+            booksOrMovies += 1
+            myTableView.reloadData()
+            buttonLabel1.titleLabel?.text = "SWITCH TO BOOKS"
+        case 2:
+            booksLoaded = false
+            booksOrMovies -= 1
+            myTableView.reloadData()
+             buttonLabel1.titleLabel?.text = "SWITCH TO MOVIES"
+        default:
+            break
+    }
+}
 
 }
+    
+
 
